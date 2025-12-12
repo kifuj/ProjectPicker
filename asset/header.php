1bd3c849ce469240793693ac9e25a4504377a106
Matteo <lamarematteo53@gmail.com>
2025-12-12 09:52:03 +0100

    feat: ajout du header sur toute les pages

--- asset/header.php
+++ asset/header.php
@@ -2,16 +2,15 @@
     <div class="logo">
         <a href="index.php">
             <img src="logo.jpg" alt="ProjectPicker">
-            <span>Project<span class="accent">Picker</span></span>
         </a>
     </div>
 
     <nav class="main-nav">
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="proposer_projet.php">Proposer un projet</a></li>
+            <li><a href="nouveau_projet.php">Proposer un projet</a></li>
             <li><a href="mes_projets.php">Mes projets</a></li>
-            <li><a href="connexion.php" class="link-login">Connexion</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
         </ul>
     </nav>
 </header>
