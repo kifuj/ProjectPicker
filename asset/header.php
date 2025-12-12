bf3641b4b0e9cba7c8f2629e63b7949da4e2b61d
Matteo <lamarematteo53@gmail.com>
2025-12-12 11:31:56 +0100

    feat: import du style du projet , style du header

--- asset/header.php
+++ asset/header.php
@@ -1,16 +1,16 @@
 <header class="site-header">
     <div class="logo">
         <a href="index.php">
-            <img src="logo.jpg" alt="ProjectPicker">
+            <img src="asset/logo.svg" alt="ProjectPicker">
         </a>
     </div>
 
     <nav class="main-nav">
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="nouveau_projet.php">Proposer un projet</a></li>
-            <li><a href="mes_projets.php">Mes projets</a></li>
-            <li><a href="connexion.php">Connexion</a></li>
+        <ul class="main-list">
+            <li><a href="index.php" class="link" >Accueil</a></li>
+            <li><a href="nouveau_projet.php" class="link">Proposer un projet</a></li>
+            <li><a href="mes_projets.php" class="link">Mes projets</a></li>
+            <li><a href="connexion.php" class="link">Connexion</a></li>
         </ul>
     </nav>
 </header>
