b696593de509681ec7d2c1d2577b3d15db610438
Matteo <lamarematteo53@gmail.com>
2025-12-12 09:46:50 +0100

    feat: ajout de la structure de la page,creation du header par le php

---
+++ asset/header.php
@@ -0,0 +1,17 @@
+<header class="site-header">
+    <div class="logo">
+        <a href="index.php">
+            <img src="logo.jpg" alt="ProjectPicker">
+            <span>Project<span class="accent">Picker</span></span>
+        </a>
+    </div>
+
+    <nav class="main-nav">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="proposer_projet.php">Proposer un projet</a></li>
+            <li><a href="mes_projets.php">Mes projets</a></li>
+            <li><a href="connexion.php" class="link-login">Connexion</a></li>
+        </ul>
+    </nav>
+</header>
